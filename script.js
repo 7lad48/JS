02eb6e30732cf17208fde3cbc152ae4c9311c1ec
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-07 14:32:38 +0300

    typeof test

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello world! script.js');
+var num = 42
+var string = 'message'
+var isTrue = true // or false
+var obj = {a: 1}
+var noth = null
+var undef = undefined
+
+console.log(typeof string)
