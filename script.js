e1367324079896e78916fdd723af5f0fdd7d470f
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-11 11:32:45 +0300

    if else practice

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentYear = 2021;
 let carName = 'Ford';
-let carYear = 2008;
+let carYear = 2017;
 let carAge = currentYear-carYear;
 if (carAge < 5) {
     console.log(carName + ' младше 5 лет');
@@ -8,4 +8,11 @@ if (carAge < 5) {
     console.log(carName + ' от 5 до 10 лет');
 } else {
     console.log('Возраст '+carName+' = '+carAge+' лет.');
+}
+// При указании в if к значению false приводят:  0, null, undefined, пустая строка (''), NaN (not a number - не я вляется номером напр. при делении на 0)
+let empty ='';
+if (empty) {
+    console.log('значение true');
+} else {
+    console.log('значение false');
 }
