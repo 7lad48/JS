04652fcd2ba55254896cd457a667ccd9c1873edd
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-11 11:17:54 +0300

    if else operators practice

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log(5>3)
-console.log(5==='5')
+let currentYear = 2021;
+let carName = 'Ford';
+let carYear = 2008;
+let carAge = currentYear-carYear;
+if (carAge < 5) {
+    console.log(carName + ' младше 5 лет');
+} else if (carAge>=5 && carAge<=10) {
+    console.log(carName + ' от 5 до 10 лет');
+} else {
+    console.log('Возраст '+carName+' = '+carAge+' лет.');
+}
