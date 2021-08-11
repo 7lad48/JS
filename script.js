786adcf14caf21cd46846b36eed36454ffceb258
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-11 14:08:46 +0300

    switch case operators

---
+++ script.js
@@ -0,0 +1,20 @@
+let carColor = 'red';
+
+/*if (carColor === 'green') {
+    console.log('car color is GREEN!');
+} else if (carColor === 'yellow'){
+    console.log('car color is YeLlOw!');
+} else if (carColor === 'red'){
+    console.log('car color is Red!');
+} else {
+    console.log('car color is undefined');
+}*/
+switch (carColor) {
+    case 'green': console.log('car color is GREeN!');
+    break
+    case 'yellow': console.log('car color is Yellow!');
+    break
+    case 'red': console.log('car color is RED!');
+    break
+    default: console.log('car color is undefined');
+}
