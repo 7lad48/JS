1ddd540eed76722172be958e2ecb8f148ff12f6c
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-14 17:45:10 +0300

    string metods...

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-let carColor = 'red';
-
-/*if (carColor === 'green') {
-    console.log('car color is GREEN!');
-} else if (carColor === 'yellow'){
-    console.log('car color is YeLlOw!');
-} else if (carColor === 'red'){
-    console.log('car color is Red!');
-} else {
-    console.log('car color is undefined');
-}*/
-switch (carColor) {
-    case 'green': console.log('car color is GREeN!');
-    break
-    case 'yellow': console.log('car color is Yellow!');
-    break
-    case 'red': console.log('car color is RED!');
-    break
-    default: console.log('car color is undefined');
-}
-var carYear = 2017
-var personYear = 2009
-
-function countAge(year){
-    var currentYear=2021
-    var result = currentYear-year
-    return result
-}
-function compareYears(year, name, compareTo) {
-    if (countAge(year) < compareTo) {
-        console.log('возраст '+name+' меньше '+compareTo+' лет.')
-    } else {
-        console.log('возраст '+name+' больше '+compareTo+' лет.')
-    }
-}
-compareYears(carYear,'авто', 7)
+var str1 = 'hello world'
+var str2 = 'hello world 2'
+console.log(str1)
+console.log(str2)
+var personName = 'Victor'
+var message = "person name is '"+personName+"'"
+console.log(message)
+var message2 = 'person name is \''+personName+'\''
+var newMessage='hello world'
+console.log(newMessage.length)
+console.log(newMessage.toLowerCase())
+console.log(newMessage.charAt())
+console.log(newMessage.indexOf('world'))
+console.log(newMessage.substr(1, 5))
+console.log(newMessage.substring(1))
