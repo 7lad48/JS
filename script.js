126497e0e1cef088edf357e7c9391ff9a039439d
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-15 12:19:57 +0300

    objects

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var cars = ['Ford','Tesla','Audi','Nissan']
-cars.push('Mazda')
-console.log(cars)
-var mazda = cars.pop()
-console.log(cars, mazda,'добавляем unshift(kia) в cars')
-cars.unshift('kia')
-console.log(cars)
-console.log(cars.indexOf('kia'))
-var kiaIndex=cars.indexOf('kia')
-var kia =cars[kiaIndex]
-console.log(kia)
+var person = {
+    name: 'Vlad',
+    year: 1995,
+    color: 'white',
+    prof: ['vr', 'pr'],
+    cat: {
+        name: 'Kisel',
+        color: 'Orange',
+        age: 2
+    }
+}
+console.log(person)
