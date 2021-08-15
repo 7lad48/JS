06d9db64421416744ed3d991a560f774ddabb9b2
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-15 11:45:52 +0300

    massive

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-var str1 = 'hello world'
-var str2 = 'hello world 2'
-console.log(str1)
-console.log(str2)
-var personName = 'Victor'
-var message = "person name is '"+personName+"'"
-console.log(message)
-var message2 = 'person name is \''+personName+'\''
-var newMessage='hello world'
-console.log(newMessage.length)
-console.log(newMessage.toLowerCase())
-console.log(newMessage.charAt())
-console.log(newMessage.indexOf('world'))
-console.log(newMessage.substr(1, 5))
-console.log(newMessage.substring(1))
+var cars = ['Ford','Tesla','Audi','Nissan']
+cars.push('Mazda')
+console.log(cars)
+var mazda = cars.pop()
+console.log(cars, mazda,'добавляем unshift(kia) в cars')
+cars.unshift('kia')
+console.log(cars)
+console.log(cars.indexOf('kia'))
+var kiaIndex=cars.indexOf('kia')
+var kia =cars[kiaIndex]
+console.log(kia)
