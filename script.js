124a711b0eddb77e1198f66e3841e8bdc1bb84ae
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-22 15:55:04 +0300

    DOM begin (old methods)

--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-//numbers.push('Not a number')
-//numbers.unshift('word')
-
-/*for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        console.log(numbers[i])
-    }
-}*/
-
-/*for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 !== 0) {
-        continue
-    }
-    console.log(numbers[i])
-}
-for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        continue
-    }
-    console.log(numbers[i])
-}*/
-var numbers = [1,2,3,'trololo',4,5,6,7,8,9]
-numbers.push('string')
-
-for (var i = 0; i < numbers.length; i++) {
-    if (typeof numbers[i] === 'string') {
-        break
-    }
-    console.log(numbers[i])
-}
-
-console.log('-------- continue продолжает цикл пропуская строки : -----------------')
-
-for (var i = 0; i < numbers.length; i++) {
-    if (typeof numbers[i] === 'string') {
-        continue
-    }
-    console.log(numbers[i])
-}
+let div = document.getElementById('playground')
+let p = document.getElementsByClassName('text')
+console.log(div)
+console.log(p)
+let h1 = document.getElementsByTagName('h1')
+console.log(h1)
