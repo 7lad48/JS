0aa49bb6dfbbdc4fb7aff66cb6b7f84f25b27c2d
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-23 18:25:15 +0300

    querySelector / All

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let div = document.getElementById('playground')
+//       Устаревшие методы
+/*let div = document.getElementById('playground')
 let p = document.getElementsByClassName('text')
 console.log(div)
 console.log(p)
 let h1 = document.getElementsByTagName('h1')
-console.log(h1)
+console.log(h1)*/
+let div = document.querySelector('#playground')
+console.log(div)
+let p = document.querySelectorAll('p')
+console.log(p)
+let ul = document.querySelector('#playground >div ul')
+console.log(ul)
