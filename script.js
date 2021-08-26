ad1bd2cd3574545059e088d4550f87ab400b8035
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-26 18:54:32 +0300

    js class manipulations

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-//       Устаревшие методы
-/*let div = document.getElementById('playground')
-let p = document.getElementsByClassName('text')
-console.log(div)
-console.log(p)
-let h1 = document.getElementsByTagName('h1')
-console.log(h1)*/
-let div = document.querySelector('#playground')
-let p = document.querySelectorAll('p')
-let h1 = document.querySelector('h1')
-div.innerHTML = '<h2 style="color: red;">From JavaScript</h2>'
-console.log(div.innerHTML)
-console.log(h1.textContent)
-h1.textContent = 'changed text from JS'
-console.log(h1.textContent) 
-let input = document.querySelector('input')
-console.log(input.placeholder)
-input.value= 'yeeeeaa'
 let a = document.querySelector('a')
 let oldHref = a.getAttribute('href')
 
 a.setAttribute('href','https://ya.ru')
 a.setAttribute('title','go to yandex')
 a.textContent='YandeX'
+
+let box1 = document.querySelector('#box1')
+let box2 = document.querySelector('#box2')
+box1.classList.add('red')
+box2.classList.remove('blue')
+var hasClass = box2.classList.contains('blue')
+console.log(hasClass)
+/*if (hasClass) {
+    box2.classList.add('blue')
+} else {
+    box2.classList.remove('blue')
+}*/
+hasClass ? box2.classList.remove('blue') : box2.classList.add('blue')
