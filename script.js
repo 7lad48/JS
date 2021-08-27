bc09af43b7f66b50ee19d65938e5339c2db95c68
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-27 23:06:34 +0300

    event.preventDefault убрать действие по умолчанию

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let divs = document.querySelectorAll('div')
-
+let link = document.querySelector('a')
 for (var i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', function(event) {
         event.stopPropagation()
         console.log(this.getAttribute('id'))
     })
 }
+
+function handleLinkClick(event) {
+    event.preventDefault()
+    let div = divs[0]
+    /*if (div.style.display === 'none') {
+        div.style.display = 'flex'
+    } else {
+        div.style.display = 'none'
+    }*/
+    div.style.display = div.style.display === 'none' 
+    ? 'flex'  
+    : 'none'
+    
+}
+
+link.addEventListener('click', handleLinkClick)
