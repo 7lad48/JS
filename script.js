374cbacfbbc5e65702cc1b5ed10470b4195af2eb
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-27 14:38:59 +0300

    .addEventListener practice begin

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-let a = document.querySelector('a')
-let oldHref = a.getAttribute('href')
+let button = document.querySelector('button')
+let h1 = document.querySelector('h1')
+let input = document.querySelector('input')
 
-a.setAttribute('href','https://ya.ru')
-a.setAttribute('title','go to yandex')
-a.textContent='YandeX'
-
-let box1 = document.querySelector('#box1')
-let box2 = document.querySelector('#box2')
-box1.classList.add('red')
-box2.classList.remove('blue')
-var hasClass = box2.classList.contains('blue')
-console.log(hasClass)
-/*if (hasClass) {
-    box2.classList.add('blue')
-} else {
-    box2.classList.remove('blue')
+/*function buttonHandler() {
+    console.log('clicked')
+    input.value !== ''||' ' ?
+    h1.textContent = input.value
+    : window.alert('текст не введён')
 }*/
-hasClass ? box2.classList.remove('blue') : box2.classList.add('blue')
+function buttonHandler() {
+    console.log('clicked')
+    if (input.value.length < 1) {
+        window.alert('текст не введён')
+    } else { 
+    h1.textContent = input.value
+    }
+}
+
+h1.addEventListener('mouseenter', function () {
+    this.style.color = 'red'
+    this.style.backgroundColor = 'green'
+    
+
+})
+h1.addEventListener('mouseleave', function () {
+    this.style.color = 'black'
+    this.style.backgroundColor = 'transparent'
+})
+
+button.addEventListener('click', buttonHandler)
