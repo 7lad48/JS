e77494732e0dbfa58c46d8309b69a685f55eafa4
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-27 22:28:14 +0300

    .addEventListener function(event) event.stopPropagation()

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let divs = document.querySelectorAll('div')
 
 for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', function() {
+    divs[i].addEventListener('click', function(event) {
+        event.stopPropagation()
         console.log(this.getAttribute('id'))
-    }, false)
+    })
 }
