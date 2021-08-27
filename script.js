559dbb96fa792feba1e11174d5c47a768903626f
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-27 21:10:20 +0300

    .addEventListener(-,-, false - погружение // true - всплытие)

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-let button = document.querySelector('button')
-let h1 = document.querySelector('h1')
-let input = document.querySelector('input')
+let divs = document.querySelectorAll('div')
 
-/*function buttonHandler() {
-    console.log('clicked')
-    input.value !== ''||' ' ?
-    h1.textContent = input.value
-    : window.alert('текст не введён')
-}*/
-function buttonHandler() {
-    console.log('clicked')
-    if (input.value.length < 1) {
-        window.alert('текст не введён')
-    } else { 
-    h1.textContent = input.value
-    }
+for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', function() {
+        console.log(this.getAttribute('id'))
+    }, false)
 }
-
-h1.addEventListener('mouseenter', function () {
-    this.style.color = 'red'
-    this.style.backgroundColor = 'green'
-    
-
-})
-h1.addEventListener('mouseleave', function () {
-    this.style.color = 'black'
-    this.style.backgroundColor = 'transparent'
-})
-
-button.addEventListener('click', buttonHandler)
