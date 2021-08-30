5efd342b357373a739e3a485094383c76fd04243
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-30 17:52:54 +0300

    alert, confirm, prompt; console. log,warn,info,error

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-/*let ps = document.querySelectorAll('p')
-for (var i = 0; i <ps.length; i++) {
-    ps[i].addEventListener('click', function(event) {
-        event.target.style.color = event.target.style.color === 'blue'
-        ? 'black'
-        : 'blue' 
-    })
-}*/
-document.getElementById('wrapper').addEventListener('click', function(event) {
-    let tagName = event.target.tagName.toLowerCase()
-    if (tagName === 'p') {
-        event.target.style.color = 'blue'
-    }
-    if (event.target.classList.contains('color')){
-        event.target.style.color = 'red'
+document.querySelector('#alert').addEventListener('click', function() {
+    alert('You click button!')
+})
+
+document.querySelector('#confirm').addEventListener('click', function() {
+    let decision = confirm('Нажать на кнопку? Вы уверены?')
+
+    console.log(decision)
+    if(decision) {
+        alert('Вы согласились хо хо')
     }
 })
+document.querySelector('#prompt').addEventListener('click', function() {
+    let age = prompt('Введите свой возраст', 18)
+    if(age >= 18) {
+        alert('Вам больше 18')
+    } else {
+        alert('еще слишком молоды')
+    }
+})
+console.warn('предупреждаю!')
+console.error('Ошибкааа!')
+console.info('информацияяя')
