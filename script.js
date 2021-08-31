a67bfdd7776464f876e30583682255fae85ed157
7lad48 <54816005+7lad48@users.noreply.github.com>
2021-08-31 14:24:46 +0300

    native JS new Data()

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-let num = 2.1
-console.log(Math.random()) // рандомное число
-console.log(Math.floor(num)) // округление в меньшую сторону, даже 2.9 == 2
-console.log(Math.ceil(num)) // округление в большую 2.1 = 3
-
-let person = {
-    name: 'Max',
-    age: 26,
-    car: {
-        model: 'Ford',
-        year: '2008',
-        color: 'Gray'
-    },
-    job: 'Frontend',
-    friends: ['Elena','Igor']
-}
-
-let strObj = JSON.stringify(person) // переводим в JSON - объект в тектовый формат JSON
-
-console.log(JSON.parse(strObj)) // обратная операция. Парсим строку в JS объект. т.ж можно в переменную сохр. готовый объект...
+let days = 10;
+let date = new Date(3600 * (24 * days) * 1000) // секунды * минуты * 24 часа * 1000 чтоб перевести в миллисекунды
+console.log(date)
+console.log(Date.now()) // получим ТАЙМШТАМП - кол-во мс с 1 января 1970 года до сейчас.
